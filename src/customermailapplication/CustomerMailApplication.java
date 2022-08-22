4c4d3f0d206ecc18dcb7c33db36986d666148578
methanon <meayrek17577@gmail.com>
2022-08-23 00:07:19 +0700

    Add existing file

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package customermailapplication;
 
 import java.util.Scanner;
 
-/**
- *
- * @author sarun
- */
+
 public class CustomerMailApplication {
 
     /**
@@ -23,8 +14,12 @@ public class CustomerMailApplication {
             case "Regular":
                 customer = new RegularCustomer();
                 break;
-            //complete MountainCustomer
-            //complete DelinquentCustomer 
+            case "Mountain":
+                customer = new MountainCustomer();
+                break;  
+            case "Delinquent":
+                customer = new DelinquentCustomer();
+                break;
         }
     }
     public String generateMail() {
